b3f1fab06001c17c423ddc5e89ef0ead673a3186
Morgan Le Guen <mo.leguen.42@gmail.com>
2025-01-25 14:38:20 +0400

    reset

--- src/auth.ts
+++
@@ -1,8 +0,0 @@
-import { SvelteKitAuth } from "@auth/sveltekit"
-import GitHub from "@auth/sveltekit/providers/github"
-import Google from "@auth/sveltekit/providers/google"
- 
-export const { handle, signIn, signOut } = SvelteKitAuth({
-  providers: [Google],
-  trustHost: true
-}) 
