13520b0ac727ecf21f66fd8e8b524719f911cbce
Morgan Le Guen <mo.leguen.42@gmail.com>
2025-01-26 12:02:33 +0400

    fixed session null

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,9 +1,9 @@
 import { redirect } from "@sveltejs/kit"
 
 import { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } from "$env/static/private";
-import { dynaScanTable, dynaDelete, dynaPut, dynaUpdate } from "$lib/server/dynaDB";
+import { dynaDelete, dynaPut, dynaUpdate } from "$lib/server/dynaDB";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { sortWorkshopAndDates, cleanTestimonies } from "../../hook.client.js";
+
 import { v4 as uuidv4 } from 'uuid';
 
 const client = new DynamoDBClient({
@@ -88,7 +88,7 @@ export const load = async ({locals, depends}) => {
   console.log("PAGE LOAD", {
     session
   })
-  if(!session.user) {
+  if(!session?.user) {
     return redirect(307, "http://localhost:5173/auth/signin?callbackUrl=http%3A%2F%2Flocalhost%3A5173%2Fadmin")
   }
 
