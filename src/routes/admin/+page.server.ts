c44324fbe82dacafa83da416d0105d858be53b0f
Morgan Le Guen <mo.leguen.42@gmail.com>
2025-01-27 10:28:40 +0400

    env urls

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,6 +1,6 @@
 import { redirect } from "@sveltejs/kit"
 
-import { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } from "$env/static/private";
+import { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, HOME_URL, LOGIN_URL } from "$env/static/private";
 import { dynaDelete, dynaPut, dynaUpdate } from "$lib/server/dynaDB";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
@@ -89,12 +89,11 @@ export const load = async ({locals, depends}) => {
     session
   })
   if(!session?.user) {
-    // "https://tendresse.vercel.app/auth/signin?callbackUrl=https%3A%2F%2Ftendresse%2Evercel%2Eapp%2Fadmin"
-    return redirect(307, "http://localhost:5173/auth/signin?callbackUrl=http%3A%2F%2Flocalhost%3A5173%2Fadmin")
+    return redirect(307, LOGIN_URL)
   }
   
   if (!["mo.leguen.42@gmail.com", "laurette.deloison@gmail.com"].includes(session?.user?.email)) {
-    return redirect(307, "http://localhost:5173/")
+    return redirect(307, HOME_URL)
   }
 
   console.log("PAGE LOAD", {
