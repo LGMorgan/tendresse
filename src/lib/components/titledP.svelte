1df3dde6dac1028b5cc49ff2e5af74b84c81a156
Other mo <othermo@mo.local>
2025-02-26 17:09:50 +0400

    /

--- src/lib/components/titledP.svelte
+++ src/lib/components/titledP.svelte
@@ -2,7 +2,7 @@
   let { title = "title", children} = $props()
 </script>
 
-<div class="mb-0 px-4 m-auto lg:w-2/3">
+<div class="mb-0 px-4 m-auto lg:w-4/5">
   <h3 class="header3 !text-left !text-[1.5em]">{title}</h3>
   <p class="p !text-left">{@render children()}</p>
   <br/>
