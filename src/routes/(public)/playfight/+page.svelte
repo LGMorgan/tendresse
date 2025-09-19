48d5134d924e089933d49ef39cfa6f78ab19dbb2
LGMorgan <mo.leguen.42@gmail.com>
2025-09-19 15:21:26 +0400

    fixed flowbite import

--- src/routes/(public)/playfight/+page.svelte
+++ src/routes/(public)/playfight/+page.svelte
@@ -2,9 +2,6 @@
   import GridDate from '$lib/components/gridDate.svelte';
   import Testimony from "$lib/components/testimony.svelte";
 	import AskMe from "$lib/components/askMe.svelte";
-  import { Button } from '$lib/components/button.svelte';
-  import { Dropdown, DropdownItem } from 'flowbite-svelte';
-  import {blur} from 'svelte/transition';
 
   const { data } = $props();
 </script>
