c70f734db524d850b31fb6741f2196fee81b2577
LGMorgan <mo.leguen.42@gmail.com>
2025-09-23 15:44:11 +0400

    kicked brevo iframe

--- src/lib/components/mailInput.svelte
+++ src/lib/components/mailInput.svelte
@@ -2,16 +2,6 @@
 	import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
-<iframe
-	title="newsletter"
-	height="680"
-	src="https://eeb30f34.sibforms.com/serve/MUIFAPlO-YTpdI5k9PMuD4-mwP2YmRSppCytCFSro-uVuHkI8TsDUz6sq_eOQxequMktSwns9045oxEPxoFAdTewVU6wswzJQM5Pi650NeUvsFdT0BLmNYAg3OgpBVhEK8K0m44wD1ckxv2vul6nV-IVAGm_sbaYPrIUaPiyF0MaHEmGuffxVs_mztcrCMoBfl37w3Hj3-6Abvws"
-	frameborder="0"
-	scrolling="auto"
-	allowfullscreen
-	style="display: block;margin-left: auto;margin-right: auto;max-width: 100%; width: 100%;"
-></iframe>
-
 <section id="newsletter">
 	<h2>Newsletter</h2>
 	<p class="text-center">Inscris-toi à la newsletter pour être informé·e des prochains ateliers.</p>
