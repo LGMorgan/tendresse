72c5fe334e68811ccd6ec4605e54727a06cc8da0
LGMorgan <mo.leguen.42@gmail.com>
2025-09-27 10:55:45 +0400

    fixed margin on main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,16 +14,14 @@
 
 <main>
 	<section>
-		<h3 class="mr-50">
+		<h3 class="m-auto md:mr-50">
 			Des espaces d’expérimentation du Consentement, du jeu et de la Tendresse à la Réunion.
 		</h3>
-		<h3 class="ml-50 text-right">
+		<h3 class="m-auto text-right md:ml-50">
 			Pour découvrir concrètement et corporellement le Consentement dans des espaces aussi sécurisés
 			et sécurisants que possible.
 		</h3>
-		<h3>
-			Mettre la Joie, le Jeu et la communication au cœur de nos quotidiens.
-		</h3>
+		<h3>Mettre la Joie, le Jeu et la communication au cœur de nos quotidiens.</h3>
 	</section>
 </main>
 
